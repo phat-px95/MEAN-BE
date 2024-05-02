74412feeb102d962fd14352549809cfb489486d3
XuanPhat <phatpx32@wru.vn>
2024-05-01 23:05:51 -0400

    Setup post request

--- apps/be-mean/src/main.ts
+++ apps/be-mean/src/main.ts
@@ -5,17 +5,32 @@
 
 import express from 'express';
 import * as path from 'path';
+import Post from '../src/model/post.model.js';
 
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requestd-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+  next();
+});
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to be-mean!' });
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body?.title,
+    content: req.body?.content
+  });
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
 });
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+// server.listen(process.env.PORT || 3000);
 server.on('error', console.error);
